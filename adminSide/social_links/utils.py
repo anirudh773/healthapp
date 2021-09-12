ec712eaabbee3967a412bbd4658fb41c24097d8f
Anirudh <yanirudha978@gmail.com>
2021-09-12 11:41:54 +0530

    social Category

--- adminSide/social_links/utils.py
+++ adminSide/social_links/utils.py
@@ -3,9 +3,15 @@ from django.utils.translation import ugettext_lazy as _
 
 MEDIUM_CHOICES = (
     ('youtube', 'Youtube'),
-    ('facebook', 'Facebook'),
-    ('linkedIn', 'LinkedIn'),
     ('pdf', 'PDF'),
     ('audio', 'Audio'),
+    ('veterinary_syllabus', 'Veterinary Syllabus'),
+    ('navle_exam', 'Navle Exam'),
+    ('books', 'Books'),
+    ('e_magazine', 'E-Magazine'),
+    ('animal_rules', 'Animal Rules'),
+    ('science_syllabus', 'Science Syllabus'),
+    ('indian_animal_organisation', 'Indian Animal Organisation'),
+    ('foreign_animal_organisation', 'Foreign Animal Organisation'),
     ('others', 'Others')
 )
